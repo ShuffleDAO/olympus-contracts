9f29c4c7f9686ef8283f19119b9a3682d7d85f13
lookingforowls <lookingforowls46@gmail.com>
2022-02-15 11:58:59 -0700

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,35 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+const MAINNET_API_KEY = process.env.RINKEBY_API_KEY;
+const RINKEBY_API_KEY = process.env.RINKEBY_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.7.5",
+  solidity: {
+    version: "0.7.5",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${RINKEBY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${MAINNET_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+     }
+   },
+    etherscan: {
+      apiKey: `${ETHERSCAN_API_KEY}`
+  }
 };
